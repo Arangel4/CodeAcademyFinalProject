c17178beceaf9df749170ee7caa3e3dfb1ceb7be
unknown <arianarangel97@gmail.com>
2020-07-13 11:54:42 -0600

    Inital commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import Login from './Login';
 import ResetPassword from './User/ResetPassword';
 import CreateUserAccount from './User/CreateUserAccount';
 import CreateGuestAccount from  './Guest/CreateGuestAccount';
+import UserDashboard from './User/UserDashboard';
+import GuestDashboard from './Guest/GuestDashboard';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
+  // Top-most components are wrapped, so my entire react app has access to the Redux store.
+  // 'store' is passed through Provider component to be access through the app.
   return (
-    <div>
-      <Router>
-        <Route path="/reset-password" component={ ResetPassword } />
-        <Route path="/create-user-account" component={ CreateUserAccount } />
-        <Route path="/guest-login" component={ CreateGuestAccount } />
-        <Route exact path="/" component={ Login } />
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Router>
+          <Route path="/reset-password" component={ ResetPassword } />
+          <Route path="/create-user-account" component={ CreateUserAccount } />
+          <Route path="/guest-login" component={ CreateGuestAccount } />
+          <Route exact path="/" component={ Login } />
+          <Route path="/user-dashboard" component={UserDashboard} />
+          <Route path="./guest-dashboard" component={GuestDashboard} />
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/Guest/GuestDashboard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const GuestDashboard = () => {
+
+}
+export default GuestDashboard;
